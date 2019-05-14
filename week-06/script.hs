e7d9ec88e8b7762277f58a3e8c408835b685ef27
Rami Gökhan Kıcı <gokhankici@gmail.com>
2019-05-13 18:14:27 -0700

    update

--- week-06/script.hs
+++ week-06/script.hs
@@ -11,9 +11,13 @@ factorial n =
   else n * factorial (n - 1)
 
 factorialT :: Int -> Int
-factorialT n = undefined
+factorialT n = go 1 1
+  where
+    go acc i = if   i > n
+               then acc
+               else go (acc * i) (i + 1)
 
 main :: IO ()
 main = do
   forM_ [0..20] $ \n ->
-    printf "factorial(%2d): %20d\n" n (factorial n)
+    printf "factorial(%2d): %20d\n" n (factorialT n)
